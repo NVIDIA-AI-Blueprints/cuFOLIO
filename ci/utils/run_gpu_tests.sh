NVIDIA-AI-Blueprints/portfolio-optimization

ci: run GPU tests and notebooks per PR, and fix the GPU lane
GPU coverage only ran post-merge on main, so a PR could break the GPU
paths and only be caught after landing. Add a blocking per-PR GPU lane.

The existing post-merge GPU lane is red, which had to be fixed first:
main.yml installs `--extra cuda13` (cuOpt 26.04) and then runs
`pytest -m gpu`, which includes the SOCP variance-cap tests. cuOpt gained
QCQP/SOCP support in 26.06, so 26.04 raises

    Exception: Quadratic constraints not supported

from QuadraticExpression.__le__. The tests guarded only on
`importorskip("cuopt")`, with no version check. Verified locally against
both cuOpt 26.04 (now skips with a reason) and 26.06 (still passes, so
the guard does not silently disable coverage).

Test fixes:
- Add tests/conftest.py with a `require_cuopt_socp` fixture that skips
  when the installed cuOpt predates 26.06.
- Apply it to test_cuopt_python_var_limit_solves_socp and
  test_socp_variance_limit_solves.

Per-PR GPU lane (pr.yaml):
- New `changes` job diffs the PR range and sets an output when
  src/, tests/, notebooks/, the GPU scripts, pyproject.toml, uv.lock or
  pr.yaml changed. Path filtering lives in a job rather than in
  `on.pull_request.paths` because a workflow-level filter makes the check
  never report, leaving a required check pending forever.
- New `gpu-tests` job runs the GPU pytest suite and executes the five
  notebooks, uploading results as artifacts.
- Guarded with `head.repo.full_name == github.repository` so the
  self-hosted GPU runner is never exposed to code from a fork.
- pr-builder now gates on it, accepting `skipped` (no GPU-relevant change,
  or a fork PR) but blocking on failure.

Deduplication:
- Extract the inline notebook and GPU-pytest bash from main.yml into
  ci/utils/run_notebooks.sh and ci/utils/run_gpu_tests.sh, now called by
  both lanes. Duplicating this per workflow is what let release-2512.yml
  silently drift. The launchable -> launchable_brev_result output name is

ci/utils/run_gpu_tests.sh
@@ -1,0 +1,51 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Run the GPU-marked pytest suite inside the CUDA container used by the GPU
+# CI lanes, from the repo root. Shared by the post-merge lane
+# (.github/workflows/main.yml) and the per-PR lane (.github/workflows/pr.yaml)
+# so the two cannot drift.
+#
+# Usage: ci/utils/run_gpu_tests.sh [CUDA_EXTRA]
+#   CUDA_EXTRA  uv extra providing the GPU stack (default: cuda13)
+#
+# Note on coverage: the `cuda13` extra tracks cuOpt 26.04, which has no
+# QCQP/SOCP support, so the variance-cap tests skip themselves there (see the
+# require_cuopt_socp fixture in tests/conftest.py). Use `cuda12` or
+# `cuda13-socp` to exercise those paths.
+
+set -euo pipefail
+
+CUDA_EXTRA="${1:-cuda13}"
+REPO_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+cd "$REPO_ROOT"
+
+echo "===================== GPU Info ====================="
+nvidia-smi
+
+echo "===================== Install uv ====================="
+if ! command -v uv >/dev/null 2>&1; then
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+  # shellcheck source=/dev/null
+  source "$HOME/.local/bin/env"
+fi
+
+echo "===================== Install Dependencies (extra: ${CUDA_EXTRA}) ====================="
+uv sync --extra "${CUDA_EXTRA}" --extra dev
+
+echo "===================== Solver versions ====================="
+uv run python -c "
+import cuopt
+print('cuopt', cuopt.__version__)
+try:
+    import cuml
+    print('cuml', cuml.__version__)
+except ImportError:
+    print('cuml NOT installed - skill benchmark tests will skip')
+"
+
+echo "===================== Run GPU test suite ====================="
+# -rs surfaces skip reasons, so a lane that silently skips everything
+# (wrong extra, missing cuML) is visible in the log instead of reading green.
+uv run pytest tests/ -v -m gpu -rs

NVIDIA-AI-Blueprints/cuFOLIO

ci/utils/run_gpu_tests.sh
@@ -1,0 +1,51 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Run the GPU-marked pytest suite inside the CUDA container used by the GPU
+# CI lanes, from the repo root. Shared by the post-merge lane
+# (.github/workflows/main.yml) and the per-PR lane (.github/workflows/pr.yaml)
+# so the two cannot drift.
+#
+# Usage: ci/utils/run_gpu_tests.sh [CUDA_EXTRA]
+#   CUDA_EXTRA  uv extra providing the GPU stack (default: cuda13)
+#
+# Note on coverage: the `cuda13` extra tracks cuOpt 26.04, which has no
+# QCQP/SOCP support, so the variance-cap tests skip themselves there (see the
+# require_cuopt_socp fixture in tests/conftest.py). Use `cuda12` or
+# `cuda13-socp` to exercise those paths.
+
+set -euo pipefail
+
+CUDA_EXTRA="${1:-cuda13}"
+REPO_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+cd "$REPO_ROOT"
+
+echo "===================== GPU Info ====================="
+nvidia-smi
+
+echo "===================== Install uv ====================="
+if ! command -v uv >/dev/null 2>&1; then
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+  # shellcheck source=/dev/null
+  source "$HOME/.local/bin/env"
+fi
+
+echo "===================== Install Dependencies (extra: ${CUDA_EXTRA}) ====================="
+uv sync --extra "${CUDA_EXTRA}" --group dev
+
+echo "===================== Solver versions ====================="
+uv run python -c "
+import cuopt
+print('cuopt', cuopt.__version__)
+try:
+    import cuml
+    print('cuml', cuml.__version__)
+except ImportError:
+    print('cuml NOT installed - skill benchmark tests will skip')
+"
+
+echo "===================== Run GPU test suite ====================="
+# -rs surfaces skip reasons, so a lane that silently skips everything
+# (wrong extra, missing cuML) is visible in the log instead of reading green.
+uv run pytest tests/ -v -m gpu -rs